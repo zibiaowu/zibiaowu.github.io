f37cbc58cf732f071bf25ff5085d56b1da2a81f6
Biao <547979729@qq.com>
2016-06-02 23:37:07 +0800

    menu bug remove

--- resource/js/main.js
+++ resource/js/main.js
@@ -67,99 +67,6 @@ function backTopFun(){
 	scrollTo(0);
 }	
 
-/*
-创建博客目录，
-id表示包含博文正文的 div 容器的 id，
-mt 和 st 分别表示主标题和次级标题的标签名称（如 H2、H3，大写或小写都可以！），
-*/
-function createBlogDirectory(id, mt, st){
-     //获取博文正文div容器
-    var elem = document.getElementById(id);
-    if(!elem) return false;
-    //获取div中所有元素结点
-    var nodes = elem.getElementsByTagName("*");
-    //创建博客目录的div容器
-    var divSideBar = document.createElement('DIV');
-    divSideBar.className = 'sideBarDirectory';
-    divSideBar.setAttribute('id', 'sideBarDirectory');
-    var divSideBarContents = document.createElement('DIV');
-    // divSideBarContents.style.display = 'none';
-    divSideBarContents.setAttribute('id', 'sideBarContents');
-    divSideBar.appendChild(divSideBarContents);
-    //创建自定义列表
-    var dlist = document.createElement("dl");
-    divSideBarContents.appendChild(dlist);
-    var num = 0;//统计找到的mt和st
-    mt = mt.toUpperCase();//转化成大写
-    st = st.toUpperCase();//转化成大写
-    //遍历所有元素结点
-    for(var i=0; i<nodes.length; i++)
-    {
-        if(nodes[i].nodeName == mt || nodes[i].nodeName == st)    
-        {
-            //获取标题文本
-            var nodetext = nodes[i].innerHTML.replace(/<\/?[^>]+>/g,"");//innerHTML里面的内容可能有HTML标签，所以用正则表达式去除HTML的标签
-            nodetext = nodetext.replace(/&nbsp;/ig, "");//替换掉所有的&nbsp;
-            // nodetext = BlogDirectory.htmlDecode(nodetext);
-            //插入锚        
-            nodes[i].setAttribute("id", "blogTitle" + num);
-            var item;
-            switch(nodes[i].nodeName)
-            {
-                case mt:    //若为主标题 
-                    item = document.createElement("dt");
-                    break;
-                case st:    //若为子标题
-                    item = document.createElement("dd");
-                    break;
-            }
-            
-            //创建锚链接
-            var itemtext = document.createTextNode(nodetext);
-            item.appendChild(itemtext);
-            item.setAttribute("name", num);
-            item.onclick = function(){        //添加鼠标点击触发函数
-                var pos = document.getElementById("blogTitle" + this.getAttribute("name")).offsetTop;
-            	scrollTo(pos);
-            };            
-            
-            //将自定义表项加入自定义列表中
-            dlist.appendChild(item);
-            num++;
-        }
-    }
-    if(num == 0){
-    	var item = document.createElement("p");
-    	var itemtext = document.createTextNode("此篇文章没有目录");
-    	item.appendChild(itemtext);
-    	dlist.appendChild(item);
-    }
-
-	// 获取放置博客目录的div容器
-    var dirBox = document.getElementById("sidebar");
-    var sidePer = document.getElementById("sidebar-Person");
-    var sidebarNavToc1 = document.getElementById("sidebar-nav-1");
-    var sidebarNavToc2 = document.getElementById("sidebar-nav-2");
-    dirBox.appendChild(divSideBar);
-    sidebarNavToc1.className = "sidebar-nav-toc-active" ;
-    sidePer.style.display = 'none' ; 
-
-    sidebarNavToc1.onclick = function () {
-    	 /* body... */ 
-    	 sidebarNavToc1.className = "sidebar-nav-toc-active" ;
-    	 sidebarNavToc2.className = "sidebar-nav-toc" ;
-    	 sidePer.style.display = 'none' ; 
-    	 divSideBar.style.display = 'block';
-    }
-    sidebarNavToc2.onclick = function () {
-    	 /* body... */ 
-    	 sidebarNavToc2.className = "sidebar-nav-toc-active" ;
-    	 sidebarNavToc1.className = "sidebar-nav-toc" ;
-    	 sidePer.style.display = 'block' ; 
-    	 divSideBar.style.display = 'none';
-    }
-} 
-
 
 //浏览器窗口大小变化时，重新定位固定栏和Sidebar
 function reloc() {	
@@ -273,8 +180,5 @@ window.onload = function() {
 
 	reloc();
 	bodyBGSize();
-	createBlogDirectory("post_main","h1","h2");
-
-	
 }
 
